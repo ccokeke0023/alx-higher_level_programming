f49a823a4a81ed5823af0607aafa96da706caf34
okeke christian <chrismalchima@gmail.com>
2024-01-26 03:22:28 -0800

    commit messsage 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    i = 0
-    length = len(argv)
+    import sys
+    i = len(sys.argv) - 1
 
-    if length == 2:
-        print("{} argument:".format(length - 1))
-    elif length == 1:
-        print("{} arguments.".format(length - 1))
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
     else:
-        print("{} arguments:".format(length - 1))
+        print("{} arguments:".format(i))
 
-    for arg in argv:
-        if i == 0:
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
             i += 1
-            continue
-        print("{}: {}".format(i, arg))
-        i += 1
